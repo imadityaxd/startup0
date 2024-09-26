396a88b2b58c20bc6d3dc677851768f931db6229
Ravish Bisht <ravishbisht85@gmail.com>
2024-09-26 12:48:27 +0530

    using modules syntax instead on common js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const dotenv = require('dotenv');
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -9,9 +9,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const startupsRouter = require('./routes/startups');
-app.use('/api/startups', startupsRouter);
+const startupsRouter = require("./routes/startups");
+app.use("/api/startups", startupsRouter);
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(5000, () => console.log('Server running on http://localhost:5000')))
-    .catch(err => console.error(err));
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(5000, () =>
+      console.log("Server running on http://localhost:5000")
+    )
+  )
+  .catch((err) => console.error(err));
