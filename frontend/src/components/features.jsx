1d1a76188b0711babf22f3ec930aff9c6345c7ee
imadityaxd <subhaiyash@gmail.com>
2024-09-27 08:33:35 +0530

    ajsdhka

--- frontend/src/components/features.jsx
+++ frontend/src/components/features.jsx
@@ -37,8 +37,7 @@ export default function Features() {
           Why Choose Us?
           </p>
           <p className="mt-6 text-lg leading-8 text-gray-600">
-            Quis tellus eget adipiscing convallis sit sit eget aliquet quis. Suspendisse eget egestas a elementum
-            pulvinar et feugiat blandit at. In mi viverra elit nunc.
+          Choose us for innovative startups with expert teams, proven success, community focus, and a commitment to sustainability and adaptability.
           </p>
         </div>
         <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
